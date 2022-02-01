6e3eaa89a6a7fec689776dae42dca1c6a50459cf
Daniel <dkilcullen25@gmail.com>
2022-02-01 10:45:06 -0600

    commit tree 27e18cd79c6c2eb0f4d7709a1c4ab93c2f0e3204

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
+    def name=name
+        @name=name
+    end
+
+    def name
+        @name
+    end
+
+    def job=job
+        @job=job
+    end
+
+    def job
+        @job
+    end
+
+    # def initialize name, job
+    #     @name=name
+    #     @job=job
+    # end
 
 end
